74a311f300f7e8682109f5b7d8c497955a57a332
Juan Lazaro <juan.lazaro2599@gmail.com>
2021-08-20 00:53:21 -0700

    allow batch from within bdt repo

--- batch.py
+++ batch.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import subprocess
 from glob import glob
@@ -6,8 +7,8 @@ from argparse import ArgumentParser
 from configparser import ConfigParser
 
 def main():
-    
-    # Syntax 
+
+    # Syntax
     parser = ArgumentParser()
     parser.add_argument('action')
     parser.add_argument('config')
@@ -22,17 +23,19 @@ def main():
     logging.info('Submitting jobs in batches of %s' % args.batch_size)
 
     # Command that will be used to submit jobs to the batch system
+    nfsdir = '/nfs/slac/g/ldmx/users/jmlazaro'
     batch_command = ('bsub '
 
-                     #+ '-W 1000 '
+                     + '-W 1000 '
 
-                     + '-q short '
-                     + '-W 10 '
+                     #+ '-q short '
+                     #+ '-W 10 '
 
                      + '-n 3 '
                      + '-R "select[centos7] span[hosts=1]" '
-                     + 'singularity run --home $PWD $PWD/ldmx_dev_latest.sif . '
-                     + 'python3 $PWD/bdt/mainframe.py '
+                     + f'singularity run --home $PWD '
+                     + f'{nfsdir}/ldmx_pro_vissig.sif . '
+                     + 'python3 mainframe.py '
                      + args.action + ' '
                      + args.config + ' '
                      + '--batch '
@@ -53,7 +56,7 @@ def main():
             if trainOrTest != '':
                 print('\nOnly running over {}ing set!!!'.format(trainOrTest))
             sets = {'test', 'train'} if trainOrTest == '' else {trainOrTest}
- 
+
             for st in sets:
 
                 infiles = sorted( glob(
@@ -61,14 +64,18 @@ def main():
                                         '/{}/*.root'.format(proc)
                     ) )
 
+                outdir = config.get( 'trees', '{}_outdir'.format(st) ) \
+                                                                  + '/' + proc
+                if not os.path.exists( outdir ):
+                    os.makedirs( outdir )
+
                 for infile in infiles:
 
                     job_commands.append(
                             batch_command
                             + '-i ' + infile + ' '
-                            + '-o '
-                            + config.get( 'trees', '{}_outdir'.format(st) ) \
-                                                                  + '/' + proc
+                            + '-o ' + outdir
+                            #+ ' -m 100'
                             )
 
         if args.action == 'eval':
@@ -77,17 +84,24 @@ def main():
                     config.get('eval', 'indir') + '/{}/*.root'.format(proc)
                     ) )
 
+            outdir = config.get( 'eval', 'outdir' ) + '/' + proc
+            if not os.path.exists( outdir ):
+                os.makedirs( outdir )
+
             for infile in infiles:
 
                 job_commands.append(
                     batch_command
                     + '-i ' + infile + ' '
-                    + '-o ' + config.get( 'eval', 'outdir' ) + '/' + proc
+                    + '-o ' + outdir
                     )
 
     if args.action == 'train':
         job_commands.append( batch_command )
-        #job_commands.append( batch_command + '-m 400') # Only for testing 
+        #job_commands.append( batch_command + '-m 400') # Only for testing
+
+    # Also for testing (a job at a time)
+    #quit( job_commands[0] )
 
     # Submit them
     for command in job_commands:
@@ -96,19 +110,20 @@ def main():
         subprocess.Popen(command, shell=True).wait()
 
         # If a batch of jobs has been submitted, don't submit another batch
-        # until all jobs are running. 
+        # until all jobs are running.
         if (job_commands.index(command) + 1)%args.batch_size == 0:
-            
+
             # Initially, wait 10s before submitting other jobs. This lets the
             # batch system catch up so we get an accurate count of pending
             # jobs.
-            sleep(10)
+            sleep(15)
 
             # Check how many jobs are pending
             cjobs = int(
                     subprocess.check_output('bjobs -p | wc -l', shell=True)
                     )
             print('cjobs: %s' % cjobs)
+            """ Enough of this i think
             while cjobs != 0:
                 logging.info('%s jobs are still pending' % cjobs)
                 sleep(30)
@@ -116,5 +131,6 @@ def main():
                         subprocess.check_output('bjobs -p | wc -l', shell=True)
                         )
                 continue
+            """
 
 if __name__ == '__main__': main()
